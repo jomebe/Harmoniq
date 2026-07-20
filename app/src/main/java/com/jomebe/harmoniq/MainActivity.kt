5089bd28b26c85de37e9e2e847aec9e836efe045
jombee <167706974+jomebe@users.noreply.github.com>
2026-07-20 13:26:55 +0900

    Fix Android CI compilation errors

--- app/src/main/java/com/jomebe/harmoniq/MainActivity.kt
+++ app/src/main/java/com/jomebe/harmoniq/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
